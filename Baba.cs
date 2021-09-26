3dde14ec9d79d8e855c4b28bfedbf7d75110b89f
Павел Семейко <pawel.semeiko@yandex.ru>
2021-09-27 00:46:37 +0300

    Добавьте файлы проекта.

---
+++ Baba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace This_end_base
+{
+    class Baba
+    {
+        public int zozo(params int[]arg)
+        {
+            int result = arg[0];
+            for (int i = 0; i < arg[0]; i++)
+            {
+                if (result < arg[i])
+                {
+                    result = arg[i];
+                }
+            }
+            return result;
+        }
+        public string zozo(string a, string b)
+        {
+            return a.Length > b.Length ? a : b;
+        }
+    }
+    class Boba : Baba
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace This_end_base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baba baba = new Baba();
+            Console.WriteLine(baba.zozo(2, 5, 123, 13, 14));
+            Console.ReadKey();
+        }
+    }
+}
